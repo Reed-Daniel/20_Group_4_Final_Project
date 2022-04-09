73008002271211a052489f7f6d9c08b358ca1cd1
Jason Klein <93780784+ChicletKeyboard@users.noreply.github.com>
2022-04-09 12:39:35 -0500

    app.py that runs flask app
    Render templates folder has not most up to date index.html

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+from Test_ML_App import lambda_handler
+
+app = Flask(__name__)
+
+@app.route('/api/stuff/<param1>', methods=['GET'])
+def stuff(param1):
+    # /api/stuff/50&200&200&100&100&5
+    if request.method == "GET":
+        inputs = param1.split('&')
+
+        #return lambda_handler(inputs)
+        return jsonify({str(i):i for i in inputs})
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
