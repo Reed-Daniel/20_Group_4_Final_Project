7555069de16b181089e1d1b5a619c0ed88fe6553
Kelly McClelland <krmcclelland@users.noreply.github.com>
2022-04-10 20:52:42 -0500

    Add express

--- templates/static/js/data2.js
+++ templates/static/js/data2.js
@@ -1,29 +1,9 @@
-//sample data to test buildtable function
-let data  = [
-    {
-      name: "Michael Jordan",
-      year: "1963",
-      position: "SG",
-      height: "77",
-      weight: "225",
-      steals: "400",
-      assists: '60'
-    },
-    {name: "Kobe Bryant",
-    year: "1978",
-    position: "SG",
-    height: "78",
-    weight: "205",
-    steals: "350",
-    assists:'70'}
-]
-
 // API gateway endpoint
-const url = 'https://f1n6f68yx1.execute-api.us-east-1.amazonaws.com/default/nba_viz';
+const url = 'C:/Users/Kelly/source/repos/Classes/20_Group_4_Final_Project/20_Group_4_Final_Project/Resources/modern_season_stats_clean.csv';
 
 
 // data in this case is = to stringprediction
-d3.json('/api/stuff').then(data=>{
+d3.json('C:/Users/Kelly/source/repos/Classes/20_Group_4_Final_Project/20_Group_4_Final_Project/Resources/modern_season_stats_clean.csv').then(data);{
   //do data stuff with 'data'
   console.log(data);
-});
+};
