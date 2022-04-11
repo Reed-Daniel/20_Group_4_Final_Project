301d7ae55f2b2553a1a72e0341d8d2b0af612a14
Ian de Jesus <ian.dejesus.md@gmail.com>
2022-04-11 09:16:06 +0900

    add axios get request

--- templates/static/js/app2.js
+++ templates/static/js/app2.js
@@ -1,30 +1,32 @@
-// import the data from data.js
-const tableData = data;
-
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
-function buildTable(data) {
+var nbadata;
+
+function buildTable() {
   // First, clear out any existing data
   tbody.html("");
 
-  // Next, loop through each object in the data
-  // and append a row and cells for each value in the row
-  data.forEach((dataRow) => {
-    // Append a row to the table body
-    let row = tbody.append("tr");
+  fetchData().then((data) => {
+    nbadata = data;
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
 
-    // Loop through each field in the dataRow and add
-    // each value as a table cell (td)
-    Object.values(dataRow).forEach((val) => {
-      let cell = row.append("td");
-      cell.text(val);
-      }
-    );
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+      });
+    });
   });
 }
 
 function handleClick() {
+  console.log(nbadata);
   // Grab the name value from the filter
   let names = d3.select("#name").property("value");
   let years = d3.select("#year").property("value");
@@ -33,60 +35,60 @@ function handleClick() {
   let weight = d3.select("#weight").property("value");
   let points = d3.select("#points").property("value");
   let assists = d3.select("#assists").property("value");
-  let filteredData = tableData;
+  let filteredData = nbadata;
 
-   // Check to see if a name was entered and filter the
+  // Check to see if a name was entered and filter the
   // data using that name.
   if (names) {
     // Apply `filter` to the table data to only keep the
     // rows where the `name` value matches the filter value
-    filteredData = filteredData.filter(row => row.name === names);
-  };
+    filteredData = filteredData.filter((row) => row.name === names);
+  }
 
   if (years) {
     // Apply `filter` to the table data to only keep the
     // rows where the `name` value matches the filter value
-    filteredData = filteredData.filter(row => row.year === years);
-  };
+    filteredData = filteredData.filter((row) => row.year === years);
+  }
 
   if (positions) {
     // Apply `filter` to the table data to only keep the
     // rows where the `name` value matches the filter value
-    filteredData = filteredData.filter(row => row.position === positions);
-  };
+    filteredData = filteredData.filter((row) => row.position === positions);
+  }
 
   if (height) {
     // Apply `filter` to the table data to only keep the
     // rows where the `name` value matches the filter value
-    filteredData = filteredData.filter(row => row.height === height);
-  };
+    filteredData = filteredData.filter((row) => row.height === height);
+  }
 
   if (weight) {
     // Apply `filter` to the table data to only keep the
     // rows where the `name` value matches the filter value
-    filteredData = filteredData.filter(row => row.weight === weight);
-  };
+    filteredData = filteredData.filter((row) => row.weight === weight);
+  }
 
   if (points) {
     // Apply `filter` to the table data to only keep the
     // rows where the `name` value matches the filter value
-    filteredData = filteredData.filter(row => row.points === points);
-  };
+    filteredData = filteredData.filter((row) => row.points === points);
+  }
 
   if (assists) {
     // Apply `filter` to the table data to only keep the
     // rows where the `name` value matches the filter value
-    filteredData = filteredData.filter(row => row.assists === assists);
-  };
+    filteredData = filteredData.filter((row) => row.assists === assists);
+  }
 
-   // Rebuild the table using the filtered data
+  // Rebuild the table using the filtered data
   // @NOTE: If no name was entered, then filteredData will
   // just be the original tableData.
   buildTable(filteredData);
-};
+}
 
 // Attach an event to listen for the form button
 d3.selectAll("#filter-btn").on("click", handleClick);
 
 // Build the table when the page loads
-buildTable(tableData);
+buildTable();
--- templates/static/js/data2.js
+++ templates/static/js/data2.js
@@ -1,31 +1,35 @@
 //sample data to test buildtable function
-let data  = [
-    {
-      name: "Michael Jordan",
-      year: "1994",
-      position: "SG",
-      height: "77",
-      weight: "225",
-      steals: "400",
-      assists: '60'
-    },
-    {name: "Kobe Bryant",
+let data = [
+  {
+    name: "Michael Jordan",
+    year: "1994",
+    position: "SG",
+    height: "77",
+    weight: "225",
+    steals: "400",
+    assists: "60",
+  },
+  {
+    name: "Kobe Bryant",
     year: "2010",
     position: "SG",
     height: "78",
     weight: "205",
     steals: "350",
-    assists:'70'}
-]
+    assists: "70",
+  },
+];
 
+const fetchData = async () => {
+  const res = await axios.get("http://localhost:4000/");
+  return res.data;
+};
 
+// // API gateway endpoint
+// const url = 'https://f1n6f68yx1.execute-api.us-east-1.amazonaws.com/default/nba_viz';
 
-// API gateway endpoint
-const url = 'https://f1n6f68yx1.execute-api.us-east-1.amazonaws.com/default/nba_viz';
-
-
-// data in this case is = to stringprediction
-d3.json('/api/stuff').then(data=>{
-  //do data stuff with 'data'
-  console.log(data);
-});
+// // data in this case is = to stringprediction
+// d3.json('/api/stuff').then(data=>{
+//   //do data stuff with 'data'
+//   console.log(data);
+// });
