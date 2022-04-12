18b007ff23431feae7acd53a67e13aa783a0224a
Kelly McClelland <krmcclelland@users.noreply.github.com>
2022-04-11 23:09:31 -0500

    Uplaad files updated

--- templates/static/js/app2.js
+++ templates/static/js/app2.js
@@ -3,24 +3,28 @@ var tbody = d3.select("tbody");
 
 var nbadata;
 
-function buildTable() {
-  // First, clear out any existing data
-  tbody.html("");
 
+function initTable() {
   fetchData().then((data) => {
     nbadata = data;
+    buildTable(nbadata);
+  })
+}
+
+function buildTable(sportsData) {
+  // First, clear out any existing data
+  tbody.html("");
     // Next, loop through each object in the data
     // and append a row and cells for each value in the row
-    data.forEach((dataRow) => {
-      // Append a row to the table body
-      let row = tbody.append("tr");
+  sportsData.forEach((dataRow) => {
+  // Append a row to the table body
+  let row = tbody.append("tr");
 
-      // Loop through each field in the dataRow and add
-      // each value as a table cell (td)
-      Object.values(dataRow).forEach((val) => {
-        let cell = row.append("td");
-        cell.text(val);
-      });
+  // Loop through each field in the dataRow and add
+  // each value as a table cell (td)
+  Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
     });
   });
 }
@@ -35,6 +39,8 @@ function handleClick() {
   let weight = d3.select("#weight").property("value");
   let points = d3.select("#points").property("value");
   let assists = d3.select("#assists").property("value");
+  let born = d3.select("#born").property("value");
+
   let filteredData = nbadata;
 
   // Check to see if a name was entered and filter the
@@ -42,44 +48,51 @@ function handleClick() {
   if (names) {
     // Apply `filter` to the table data to only keep the
     // rows where the `name` value matches the filter value
-    filteredData = filteredData.filter((row) => row.name === names);
+    filteredData = filteredData.filter((row) => row.Player == names);
+  }
+
+  if (born) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `name` value matches the filter value
+    filteredData = filteredData.filter((row) => row.born == born);
   }
 
   if (years) {
     // Apply `filter` to the table data to only keep the
     // rows where the `name` value matches the filter value
-    filteredData = filteredData.filter((row) => row.year === years);
+    filteredData = filteredData.filter((row) => row.Year == years);
   }
 
   if (positions) {
     // Apply `filter` to the table data to only keep the
     // rows where the `name` value matches the filter value
-    filteredData = filteredData.filter((row) => row.position === positions);
+    filteredData = filteredData.filter((row) => row.Position == positions);
   }
 
   if (height) {
     // Apply `filter` to the table data to only keep the
     // rows where the `name` value matches the filter value
-    filteredData = filteredData.filter((row) => row.height === height);
+    filteredData = filteredData.filter((row) => row.height == height);
   }
   if (weight) {
     // Apply `filter` to the table data to only keep the
     // rows where the `name` value matches the filter value
-    filteredData = filteredData.filter((row) => row.weight === weight);
+    filteredData = filteredData.filter((row) => row.weight == weight);
   }
 
   if (points) {
     // Apply `filter` to the table data to only keep the
     // rows where the `name` value matches the filter value
-    filteredData = filteredData.filter((row) => row.points === points);
+    filteredData = filteredData.filter((row) => row.PTS >= points);
   }
 
   if (assists) {
     // Apply `filter` to the table data to only keep the
     // rows where the `name` value matches the filter value
-    filteredData = filteredData.filter((row) => row.assists === assists);
+    filteredData = filteredData.filter((row) => row.AST >= assists);
   }
 
+  console.log("filteredData:", filteredData);
   // Rebuild the table using the filtered data
   // @NOTE: If no name was entered, then filteredData will
   // just be the original tableData.
@@ -89,5 +102,7 @@ function handleClick() {
 // Attach an event to listen for the form button
 d3.selectAll("#filter-btn").on("click", handleClick);
 
-// Build the table when the page loads
-buildTable();
+
+// initiate the table when the page loads
+initTable(); //fetch initial list of data
+
